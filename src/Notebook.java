68d9df2c885fb9ad7edbe0f49ce1cb3aba4494bf
Darcy <hq_nerv@outlook.com>
2025-08-16 23:34:59 +0800

    NULL

---
+++ src/Notebook.java
@@ -0,0 +1,33 @@
+//Code comes from Weng Kai (ZJU) 's courses in MOOC.
+//A sample of Arraylist in Java.
+import java.util.ArrayList;
+
+public class Notebook
+    {
+        private ArrayList<String> notes = new ArrayList<String>();
+
+        public void add(String s)
+            {
+
+            }
+
+        public int GetSize()
+            {
+                return 0;
+            }
+
+        public String GetNote(int index)
+            {
+                return "";
+            }
+
+        public boolean RemoveNote(int index)
+            {
+                return true;
+            }
+
+        public static void main(String[] args)
+            {
+
+            }
+    }
