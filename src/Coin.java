c15f2b5fc0d822853fa1b64245a22c037547fbbb
Darcy <hq_nerv@outlook.com>
2025-08-18 16:47:40 +0800

    NULL

--- src/Coin.java
+++ src/Coin.java
@@ -1,8 +1,9 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Coin
     {
-        private HashMap<Integer, String> coinnames = new HashMap<Integer, String>();
+        public HashMap<Integer, String> coinnames = new HashMap<Integer, String>();
 
         public Coin()
             {
@@ -10,12 +11,36 @@ public class Coin
                 coinnames.put(10, "dime");
                 coinnames.put(25, "quarter");
                 coinnames.put(50, "half-dollar");
+                coinnames.put(50, "五毛");
+                System.out.println(coinnames.keySet());
+                System.out.println(coinnames.keySet().size());
             }
 
         public String GetName(int amount)
             {
-                return coinnames.get(amount);
+                if (coinnames.containsKey(amount))
+                    {
+                        return coinnames.get(amount);
+                    } else
+                    {
+                        return "We don't have such a name.";
+                    }
             }
 
 
+        public static void main(String[] args)
+            {
+                Scanner in = new Scanner(System.in);
+                int amount = in.nextInt();
+
+                Coin coins = new Coin();
+                String name = coins.GetName(amount);
+                System.out.println(name);
+                System.out.println(coins.coinnames);
+                for (Integer i : coins.coinnames.keySet())
+                    {
+                        String s = coins.coinnames.get(i);
+                        System.out.println(s);
+                    }
+            }
     }
