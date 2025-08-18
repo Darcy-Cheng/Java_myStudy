43cceaa70e79cbef1050ea9481d2e8e74c252a65
Darcy <hq_nerv@outlook.com>
2025-08-18 13:17:41 +0800

    NULL

--- src/Notebook.java
+++ src/Notebook.java
@@ -2,6 +2,7 @@
 //A sample of Arraylist in Java.
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Notebook
     {
@@ -12,6 +13,11 @@ public class Notebook
                 notes.add(s);
             }
 
+        public void add(int location, String s)
+            {
+                notes.add(location, s);
+            }
+
         public int GetSize()
             {
                 return notes.size();
@@ -19,12 +25,23 @@ public class Notebook
 
         public String GetNote(int index)
             {
-                return "";
+                return notes.get(index);
             }
 
-        public boolean RemoveNote(int index)
+        public String[] list()
             {
-                return true;
+                String[] a = new String[notes.size()];
+//                for (int i = 0; i < notes.size(); i++)
+//                    {
+//                        a[i] = notes.get(i);
+//                    }
+                notes.toArray(a);
+                return a;
+            }
+
+        public void RemoveNote(int index)
+            {
+                notes.remove(index);
             }
 
         public static void main(String[] args)
@@ -32,6 +49,24 @@ public class Notebook
                 Notebook note1 = new Notebook();
                 note1.add("First");
                 note1.add("Second");
+                note1.add(1, "Test");
+                System.out.println(note1.GetSize());
+//                System.out.println(note1.GetNote(1));
+//                System.out.println(note1.GetNote(2));
+//                note1.RemoveNote(1);
+//                String[] a = note1.list();
+//                for (String s : a)
+//                    {
+//                        System.out.println(s);
+//                    }
+                HashSet<String> s = new HashSet<String>();
+                s.add("First");
+                s.add("Second");
+                s.add("First");
 
+                for (String a : s)
+                    {
+                        System.out.println(a);
+                    }
             }
     }
