e7d2d3e1220fb37867e388d3dfe9be0567e8ae3a
Darcy <hq_nerv@outlook.com>
2025-08-18 17:11:19 +0800

    NULL

---
+++ src/Database.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Database
+    {
+        private ArrayList<myCD> listCD = new ArrayList<myCD>();
+
+        public static void main(String[] args)
+            {
+
+            }
+
+    }
--- src/Dome/Database.java
+++
@@ -1,9 +0,0 @@
-package Dome;
-
-import java.util.ArrayList;
-
-public class Database
-    {
-        public ArrayList<myCD> listCD = new ArrayList<myCD>();
-
-    }
